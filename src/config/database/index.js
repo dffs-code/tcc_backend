fa30d92f42459a18842e3d369bfceb89ed675211
Daniel <d.soares@aluno.ifsp.edu.br>
2021-09-30 00:05:18 -0300

    change db config to heroku

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -1,9 +1,10 @@
 module.exports = {
   dialect: 'mysql',
-  host: '127.0.0.1',
-  username: 'root',
-  password: 'admin',
-  database: 'tcc',
+  host: process.env.MYSQL_HOST || 'us-cdbr-east-04.cleardb.com',
+  username: process.env.MYSQL_USERNAME || 'b12ce60e964ed7',
+  password: process.env.MYSQL_PASSWORD || '6a520526',
+  database: process.env.MYSQL_DATABASE || 'heroku_a1806d793618aee',
+  port: process.env.MYSQL_PORT || 3306,//3306
   define: {
     timestamps: true,
   },
