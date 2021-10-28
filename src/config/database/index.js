19fa681333522ec93baf7670adb49d712a07b629
Daniel <d.soares@aluno.ifsp.edu.br>
2021-10-28 18:02:53 -0300

    remove credenciais antigas

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -1,10 +1,10 @@
 module.exports = {
   dialect: 'mysql',
-  host: process.env.MYSQL_HOST || 'us-cdbr-east-04.cleardb.com',
-  username: process.env.MYSQL_USERNAME || 'b12ce60e964ed7',
-  password: process.env.MYSQL_PASSWORD || '6a520526',
-  database: process.env.MYSQL_DATABASE || 'heroku_a1806d793618aee',
-  port: process.env.MYSQL_PORT || 3306,
+  host: process.env.MYSQL_HOST,
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
   define: {
     timestamps: true,
     },
