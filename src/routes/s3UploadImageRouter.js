76b0943361de4ea497109992c26ce3b9e657d34a
Daniel <d.soares@aluno.ifsp.edu.br>
2021-11-18 23:39:48 -0300

    modifica s3 rota

--- src/routes/s3UploadImageRouter.js
+++ src/routes/s3UploadImageRouter.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const UploadImageController = require("../controllers/UploadImageController")
+const UploadImageController = require("../controllers/uploadImageController")
 const authMiddleware = require("../middlewares/auth");
 
 const router = Router();
