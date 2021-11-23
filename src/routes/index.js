7c8a9c3a85aa4c0874b465c21aa317daff5a5974
Daniel <d.soares@aluno.ifsp.edu.br>
2021-11-23 17:17:36 -0300

    remove import gentoken

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,6 @@ const category = require("./categoryRouter");
 const card = require("./cardRouter");
 const rating = require("./ratingRouter");
 const s3 = require("./s3UploadImageRouter");
-const generateToken = require("./generateToken")
 
 module.exports = (app) => {
   app.use(
