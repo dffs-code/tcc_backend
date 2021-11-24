1cc13c33756e350dae89a924a23c2e7c00230328
Daniel <d.soares@aluno.ifsp.edu.br>
2021-11-24 00:29:48 -0300

    remove auth s3 upload

--- src/routes/s3UploadImageRouter.js
+++ src/routes/s3UploadImageRouter.js
@@ -5,6 +5,6 @@ const authMiddleware = require("../middlewares/auth");
 const router = Router();
 
 router
-  .get("/s3UploadImage", authMiddleware, UploadImageController.generate)
+  .get("/s3UploadImage", UploadImageController.generate)
 
 module.exports = router;
